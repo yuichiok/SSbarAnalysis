44b7ebceae723d2e927035fa6c0d5d808a1e0b35
yuichiok <yuichi.okugawa@cern.ch>
2022-09-04 01:19:44 +0200

    output root files in output folder

--- NtupleProcessor/src/NtupleProcessor.cc
+++ NtupleProcessor/src/NtupleProcessor.cc
@@ -18,7 +18,7 @@ NtupleProcessor::NtupleProcessor()
 
     TString dummy_label = "rv02-02.sv02-02.mILD_l5_o1_v02.E250-SetA.I500010.P2f_z_h.eL.pR.n002.d_dstm_15162_000";
 
-    NtupleFile = TFile::Open(dummy_label + ".tmp.root", "RECREATE");
+    NtupleFile = TFile::Open("rootfiles/" + dummy_label + ".tmp.root", "RECREATE");
 
   // Takes input options and processes appropriately
   //   Options that can be specified:
