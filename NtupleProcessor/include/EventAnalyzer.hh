61cb13e159a16dcad19d7e1c74f694d029250dd5
yuichiok <yuichi.okugawa@cern.ch>
2022-09-05 23:51:19 +0200

    format branch map

--- NtupleProcessor/include/EventAnalyzer.hh
+++ NtupleProcessor/include/EventAnalyzer.hh
@@ -30,18 +30,22 @@ class EventAnalyzer
   // methods
     bool mapTree(TTree*);                         // Maps class variables to an input TTree.
     void evalCriteria();                          // Evaluates the class' list of event selection criteria
-    // virtual Bool_t   Notify();
+    virtual Bool_t   Notify();
 
 
   // Running Variables
-    TString options;         // Options input with TreeIterator.
+    TString  options;  // Options input with TreeIterator.
+    TTree   *fChain;   //!pointer to the analyzed TTree or TChain
+    Int_t    fCurrent; //!current Tree number in a TChain
 
-    // TTree          *fChain;   //!pointer to the analyzed TTree or TChain
-    // Int_t           fCurrent; //!current Tree number in a TChain
+  // Extra data
+    long patEventsAnalyzed;     // Number of events that were processed to make the Ntuple.
+    long entriesInNtuple  ;     // Number of events that were processed to make the Ntuple.
 
-// Fixed size dimensions of array or collections stored in the TTree if any.
+  // Fixed size dimensions of array or collections stored in the TTree if any.
 
   // Declaration of leaf types
+  // MC VARIABLES
     Float_t mc_quark_E[NQUARKS];        Int_t   mc_quark_ps_n;                        Int_t   mc_stable_n;
     Float_t mc_quark_px[NQUARKS];       Float_t mc_quark_ps_E[MAX_NPARTICLES];        Float_t mc_stable_E[MAX_NPARTICLES];
     Float_t mc_quark_py[NQUARKS];       Float_t mc_quark_ps_px[MAX_NPARTICLES];       Float_t mc_stable_px[MAX_NPARTICLES];
@@ -61,6 +65,7 @@ class EventAnalyzer
                                                                                       Float_t mc_stable_jet_py[NJETS];
                                                                                       Float_t mc_stable_jet_pz[NJETS];
 
+  // JET VARIABLES
     Float_t truejet_E[NTRUE_JETS];      Float_t jet_E[NJETS];       Float_t y23;          Float_t major_thrust_value;       Float_t major_thrust_axis[3];
     Float_t truejet_px[NTRUE_JETS];     Float_t jet_px[NJETS];      Float_t y12;          Float_t minor_thrust_value;       Float_t minor_thrust_axis[3];
     Float_t truejet_py[NTRUE_JETS];     Float_t jet_py[NJETS];      Float_t d23;          Float_t principle_thrust_value;   Float_t principle_thrust_axis[3];
@@ -68,92 +73,91 @@ class EventAnalyzer
     Int_t   truejet_type[NTRUE_JETS];   Float_t jet_btag[NJETS];    Float_t oblateness; 
     Int_t   truejet_pdg[NTRUE_JETS];    Float_t jet_ctag[NJETS];    Float_t aplanarity; 
     
-    
-
-    Int_t           pfo_n;
-    Int_t           jet_nvtx;
-    Int_t           pfo_n_j1;
-    Int_t           jet_nvtx_j1;
-    Int_t           pfo_n_j2;
-    Int_t           jet_nvtx_j2;
-    Int_t           pfo_match[MAX_NPARTICLES];   //[pfo_n]
-    Int_t           pfo_truejet_pdg[MAX_NPARTICLES];   //[pfo_n]
-    Int_t           pfo_truejet_type[MAX_NPARTICLES];   //[pfo_n]
-    Int_t           pfo_pdgcheat[MAX_NPARTICLES];   //[pfo_n]
-    Int_t           pfo_nparents[MAX_NPARTICLES];   //[pfo_n]
-    Int_t           pfo_pdgcheat_parent[MAX_NPARTICLES][MAX_NPARTICLES];   //[pfo_n]
-    Float_t         pfo_E[MAX_NPARTICLES];   //[pfo_n]
-    Float_t         pfo_E_calo[MAX_NPARTICLES];   //[pfo_n]
-    Float_t         pfo_px[MAX_NPARTICLES];   //[pfo_n]
-    Float_t         pfo_py[MAX_NPARTICLES];   //[pfo_n]
-    Float_t         pfo_pz[MAX_NPARTICLES];   //[pfo_n]
-    Float_t         pfo_m[MAX_NPARTICLES];   //[pfo_n]
-    Int_t           pfo_type[MAX_NPARTICLES];   //[pfo_n]
-    Int_t           pfo_isoverlay[MAX_NPARTICLES];   //[pfo_n]
-    Int_t           pfo_isisr[MAX_NPARTICLES];   //[pfo_n]
-    Int_t           pfo_vtx[MAX_NPARTICLES];   //[pfo_n]
-    Int_t           pfo_charge[MAX_NPARTICLES];   //[pfo_n]
-    Int_t           pfo_ntracks[MAX_NPARTICLES];   //[pfo_n]
-    Int_t           pfo_tpc_hits[MAX_NPARTICLES];   //[pfo_n]
-    Float_t         pfo_dedx[MAX_NPARTICLES];   //[pfo_n]
-    Float_t         pfo_dedxerror[MAX_NPARTICLES];   //[pfo_n]
-    Float_t         pfo_d0[MAX_NPARTICLES];   //[pfo_n]
-    Float_t         pfo_d0error[MAX_NPARTICLES];   //[pfo_n]
-    Float_t         pfo_z0[MAX_NPARTICLES];   //[pfo_n]
-    Float_t         pfo_z0error[MAX_NPARTICLES];   //[pfo_n]
-    Float_t         pfo_phi[MAX_NPARTICLES];   //[pfo_n]
-    Float_t         pfo_phierror[MAX_NPARTICLES];   //[pfo_n]
-    Float_t         pfo_omega[MAX_NPARTICLES];   //[pfo_n]
-    Float_t         pfo_omegaerror[MAX_NPARTICLES];   //[pfo_n]
-    Float_t         pfo_tanlambda[MAX_NPARTICLES];   //[pfo_n]
-    Float_t         pfo_tanlambdaerror[MAX_NPARTICLES];   //[pfo_n]
-    Float_t         pfo_chi2[MAX_NPARTICLES];   //[pfo_n]
-    Float_t         pfo_ndf[MAX_NPARTICLES];   //[pfo_n]
-    Float_t         pfo_vtxpt[MAX_NPARTICLES][3];   //[pfo_n]
-    Float_t         pfo_endpt[MAX_NPARTICLES][3];   //[pfo_n]
-    Int_t           pfo_pid[MAX_NPARTICLES];   //[pfo_n]
-    Float_t         pfo_pid_likelihood[MAX_NPARTICLES];   //[pfo_n]
-    Float_t         pfo_pid_eprob[MAX_NPARTICLES];   //[pfo_n]
-    Float_t         pfo_pid_muprob[MAX_NPARTICLES];   //[pfo_n]
-    Float_t         pfo_pid_piprob[MAX_NPARTICLES];   //[pfo_n]
-    Float_t         pfo_pid_kprob[MAX_NPARTICLES];   //[pfo_n]
-    Float_t         pfo_pid_pprob[MAX_NPARTICLES];   //[pfo_n]
-    Float_t         pfo_pid_hprob[MAX_NPARTICLES];   //[pfo_n]
-    Int_t           pfo_piddedx[MAX_NPARTICLES];   //[pfo_n]
-    Float_t         pfo_piddedx_likelihood[MAX_NPARTICLES];   //[pfo_n]
-    Float_t         pfo_piddedx_eprob[MAX_NPARTICLES];   //[pfo_n]
-    Float_t         pfo_piddedx_muprob[MAX_NPARTICLES];   //[pfo_n]
-    Float_t         pfo_piddedx_piprob[MAX_NPARTICLES];   //[pfo_n]
-    Float_t         pfo_piddedx_kprob[MAX_NPARTICLES];   //[pfo_n]
-    Float_t         pfo_piddedx_pprob[MAX_NPARTICLES];   //[pfo_n]
-    Float_t         pfo_piddedx_hprob[MAX_NPARTICLES];   //[pfo_n]
-    Float_t         pfo_piddedx_e_dedxdist[MAX_NPARTICLES];   //[pfo_n]
-    Float_t         pfo_piddedx_mu_dedxdist[MAX_NPARTICLES];   //[pfo_n]
-    Float_t         pfo_piddedx_pi_dedxdist[MAX_NPARTICLES];   //[pfo_n]
-    Float_t         pfo_piddedx_k_dedxdist[MAX_NPARTICLES];   //[pfo_n]
-    Float_t         pfo_piddedx_p_dedxdist[MAX_NPARTICLES];   //[pfo_n]
-    Float_t         pfo_piddedx_e_lkhood[MAX_NPARTICLES];   //[pfo_n]
-    Float_t         pfo_piddedx_mu_lkhood[MAX_NPARTICLES];   //[pfo_n]
-    Float_t         pfo_piddedx_pi_lkhood[MAX_NPARTICLES];   //[pfo_n]
-    Float_t         pfo_piddedx_k_lkhood[MAX_NPARTICLES];   //[pfo_n]
-    Float_t         pfo_piddedx_p_lkhood[MAX_NPARTICLES];   //[pfo_n]
-    Float_t         pfo_pidtof_p_at_calo[MAX_NPARTICLES];   //[pfo_n]
-    Float_t         pfo_pidtof_closest_beta_0ps[MAX_NPARTICLES];   //[pfo_n]
-    Float_t         pfo_pidtof_closest_beta_10ps[MAX_NPARTICLES];   //[pfo_n]
-    Float_t         pfo_pidtof_closest_beta_50ps[MAX_NPARTICLES];   //[pfo_n]
-    Float_t         pfo_pidtof_closest_beta_100ps[MAX_NPARTICLES];   //[pfo_n]
-    Float_t         pfo_pidtof_fastest_beta_0ps[MAX_NPARTICLES];   //[pfo_n]
-    Float_t         pfo_pidtof_fastest_beta_10ps[MAX_NPARTICLES];   //[pfo_n]
-    Float_t         pfo_pidtof_fastest_beta_50ps[MAX_NPARTICLES];   //[pfo_n]
-    Float_t         pfo_pidtof_fastest_beta_100ps[MAX_NPARTICLES];   //[pfo_n]
-    Float_t         pfo_pidtof_cylfit_beta_0ps[MAX_NPARTICLES];   //[pfo_n]
-    Float_t         pfo_pidtof_cylfit_beta_10ps[MAX_NPARTICLES];   //[pfo_n]
-    Float_t         pfo_pidtof_cylfit_beta_50ps[MAX_NPARTICLES];   //[pfo_n]
-    Float_t         pfo_pidtof_cylfit_beta_100ps[MAX_NPARTICLES];   //[pfo_n]
-    Float_t         pfo_pidtof_closestfit_beta_0ps[MAX_NPARTICLES];   //[pfo_n]
-    Float_t         pfo_pidtof_closestfit_beta_10ps[MAX_NPARTICLES];   //[pfo_n]
-    Float_t         pfo_pidtof_closestfit_beta_50ps[MAX_NPARTICLES];   //[pfo_n]
-    Float_t         pfo_pidtof_closestfit_beta_100ps[MAX_NPARTICLES];   //[pfo_n]
+  // PFO VARIABLES
+    Int_t pfo_n;
+    Int_t jet_nvtx;
+    Int_t pfo_n_j1;
+    Int_t jet_nvtx_j1;
+    Int_t pfo_n_j2;
+    Int_t jet_nvtx_j2;
+    Int_t pfo_match[MAX_NPARTICLES];
+    Int_t pfo_truejet_pdg[MAX_NPARTICLES];
+    Int_t pfo_truejet_type[MAX_NPARTICLES];
+    Int_t pfo_pdgcheat[MAX_NPARTICLES];
+    Int_t pfo_nparents[MAX_NPARTICLES];
+    Int_t pfo_pdgcheat_parent[MAX_NPARTICLES][MAX_NPARTICLES];
+    Float_t pfo_E[MAX_NPARTICLES];
+    Float_t pfo_E_calo[MAX_NPARTICLES];
+    Float_t pfo_px[MAX_NPARTICLES];
+    Float_t pfo_py[MAX_NPARTICLES];
+    Float_t pfo_pz[MAX_NPARTICLES];
+    Float_t pfo_m[MAX_NPARTICLES];
+    Int_t pfo_type[MAX_NPARTICLES];
+    Int_t pfo_isoverlay[MAX_NPARTICLES];
+    Int_t pfo_isisr[MAX_NPARTICLES];
+    Int_t pfo_vtx[MAX_NPARTICLES];
+    Int_t pfo_charge[MAX_NPARTICLES];
+    Int_t pfo_ntracks[MAX_NPARTICLES];
+    Int_t pfo_tpc_hits[MAX_NPARTICLES];
+    Float_t pfo_dedx[MAX_NPARTICLES];
+    Float_t pfo_dedxerror[MAX_NPARTICLES];
+    Float_t pfo_d0[MAX_NPARTICLES];
+    Float_t pfo_d0error[MAX_NPARTICLES];
+    Float_t pfo_z0[MAX_NPARTICLES];
+    Float_t pfo_z0error[MAX_NPARTICLES];
+    Float_t pfo_phi[MAX_NPARTICLES];
+    Float_t pfo_phierror[MAX_NPARTICLES];
+    Float_t pfo_omega[MAX_NPARTICLES];
+    Float_t pfo_omegaerror[MAX_NPARTICLES];
+    Float_t pfo_tanlambda[MAX_NPARTICLES];
+    Float_t pfo_tanlambdaerror[MAX_NPARTICLES];
+    Float_t pfo_chi2[MAX_NPARTICLES];
+    Float_t pfo_ndf[MAX_NPARTICLES];
+    Float_t pfo_vtxpt[MAX_NPARTICLES][3];
+    Float_t pfo_endpt[MAX_NPARTICLES][3];
+    Int_t pfo_pid[MAX_NPARTICLES];
+    Float_t pfo_pid_likelihood[MAX_NPARTICLES];
+    Float_t pfo_pid_eprob[MAX_NPARTICLES];
+    Float_t pfo_pid_muprob[MAX_NPARTICLES];
+    Float_t pfo_pid_piprob[MAX_NPARTICLES];
+    Float_t pfo_pid_kprob[MAX_NPARTICLES];
+    Float_t pfo_pid_pprob[MAX_NPARTICLES];
+    Float_t pfo_pid_hprob[MAX_NPARTICLES];
+    Int_t pfo_piddedx[MAX_NPARTICLES];
+    Float_t pfo_piddedx_likelihood[MAX_NPARTICLES];
+    Float_t pfo_piddedx_eprob[MAX_NPARTICLES];
+    Float_t pfo_piddedx_muprob[MAX_NPARTICLES];
+    Float_t pfo_piddedx_piprob[MAX_NPARTICLES];
+    Float_t pfo_piddedx_kprob[MAX_NPARTICLES];
+    Float_t pfo_piddedx_pprob[MAX_NPARTICLES];
+    Float_t pfo_piddedx_hprob[MAX_NPARTICLES];
+    Float_t pfo_piddedx_e_dedxdist[MAX_NPARTICLES];
+    Float_t pfo_piddedx_mu_dedxdist[MAX_NPARTICLES];
+    Float_t pfo_piddedx_pi_dedxdist[MAX_NPARTICLES];
+    Float_t pfo_piddedx_k_dedxdist[MAX_NPARTICLES];
+    Float_t pfo_piddedx_p_dedxdist[MAX_NPARTICLES];
+    Float_t pfo_piddedx_e_lkhood[MAX_NPARTICLES];
+    Float_t pfo_piddedx_mu_lkhood[MAX_NPARTICLES];
+    Float_t pfo_piddedx_pi_lkhood[MAX_NPARTICLES];
+    Float_t pfo_piddedx_k_lkhood[MAX_NPARTICLES];
+    Float_t pfo_piddedx_p_lkhood[MAX_NPARTICLES];
+    Float_t pfo_pidtof_p_at_calo[MAX_NPARTICLES];
+    Float_t pfo_pidtof_closest_beta_0ps[MAX_NPARTICLES];
+    Float_t pfo_pidtof_closest_beta_10ps[MAX_NPARTICLES];
+    Float_t pfo_pidtof_closest_beta_50ps[MAX_NPARTICLES];
+    Float_t pfo_pidtof_closest_beta_100ps[MAX_NPARTICLES];
+    Float_t pfo_pidtof_fastest_beta_0ps[MAX_NPARTICLES];
+    Float_t pfo_pidtof_fastest_beta_10ps[MAX_NPARTICLES];
+    Float_t pfo_pidtof_fastest_beta_50ps[MAX_NPARTICLES];
+    Float_t pfo_pidtof_fastest_beta_100ps[MAX_NPARTICLES];
+    Float_t pfo_pidtof_cylfit_beta_0ps[MAX_NPARTICLES];
+    Float_t pfo_pidtof_cylfit_beta_10ps[MAX_NPARTICLES];
+    Float_t pfo_pidtof_cylfit_beta_50ps[MAX_NPARTICLES];
+    Float_t pfo_pidtof_cylfit_beta_100ps[MAX_NPARTICLES];
+    Float_t pfo_pidtof_closestfit_beta_0ps[MAX_NPARTICLES];
+    Float_t pfo_pidtof_closestfit_beta_10ps[MAX_NPARTICLES];
+    Float_t pfo_pidtof_closestfit_beta_50ps[MAX_NPARTICLES];
+    Float_t pfo_pidtof_closestfit_beta_100ps[MAX_NPARTICLES];
 
     // List of branches
     TBranch        *b_mc_quark_E;   //!
@@ -314,10 +318,6 @@ class EventAnalyzer
     TBranch        *b_pfo_pidtof_closestfit_beta_10ps;   //!
     TBranch        *b_pfo_pidtof_closestfit_beta_50ps;   //!
     TBranch        *b_pfo_pidtof_closestfit_beta_100ps;   //!
-
-  // Extra data
-    long patEventsAnalyzed;     // Number of events that were processed to make the Ntuple.
-    long entriesInNtuple  ;     // Number of events that were processed to make the Ntuple.
 
   private: 
 
