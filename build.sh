d015273d66ab89119476a1b6e730a1ed7bb1c0de
yuichiok <yuichi.okugawa@cern.ch>
2022-09-08 14:52:14 +0200

    shape up the buid with clean option

--- build.sh
+++ build.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-if [[ -d build ]]
+if [[ -d "build" ]]
 then
     rm -rf build/
     echo "rm -rf build/"
 fi
 
+if [[ -f "main.exe" ]]
+then
+    rm -rf main.exe
+    echo "rm -rf main.exe"
+fi
+
 mkdir build
 cd build
-# cmake -DCMAKE_C_COMPILER=/usr/bin/gcc -DCMAKE_CXX_COMPILER=/usr/bin/g++ ..
-# cmake -DCMAKE_C_COMPILER=/pbs/software/centos-7-x86_64/gcc/7.3.0/bin/gcc -DCMAKE_CXX_COMPILER=/pbs/software/centos-7-x86_64/gcc/7.3.0/bin/g++ ..
 cmake ..
-make VERBOSE=1
+# make VERBOSE=1
+make
 cd -
