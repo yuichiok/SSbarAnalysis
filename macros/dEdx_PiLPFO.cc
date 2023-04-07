811de547b16f700762f70271e094d996e4577376
yuichiok <okugawa@epx.phys.tohoku.ac.jp>
2023-04-07 13:49:48 +0200

    change file names

--- macros/dEdx_PiLPFO.cc
+++ macros/dEdx_PiLPFO.cc
@@ -245,8 +245,8 @@ void dEdx_PiLPFO()
 {
   gStyle->SetOptStat(0);
 
-  // TFile *uu_file = new TFile("../rootfiles/merged/rv02-02.sv02-02.mILD_l5_o1_v02.E250-SetA.I500010.P2f_z_h.eL.pR.uu.PiLPFO.PFOp15.LPFOp15_pNaN.tpc0.hists.all.root","READ");
-  TFile *uu_file = new TFile("../rootfiles/merged/rv02-02.sv02-02.mILD_l5_o1_v02.E250-SetA.I500010.P2f_z_h.eL.pR.ss.PiLPFO.PFOp15.LPFOp15_pNaN.tpc0.hists.all.root","READ");
+  TFile *uu_file = new TFile("../rootfiles/merged/rv02-02.sv02-02.mILD_l5_o1_v02.E250-SetA.I500010.P2f_z_h.eL.pR.uu.PiLPFO.PFOp15.LPFOp15_pNaN.tpc0.hists.all.root","READ");
+  // TFile *uu_file = new TFile("../rootfiles/merged/rv02-02.sv02-02.mILD_l5_o1_v02.E250-SetA.I500010.P2f_z_h.eL.pR.ss.PiLPFO.PFOp15.LPFOp15_pNaN.tpc0.hists.all.root","READ");
 
   dEdx_p(uu_file);
   dEdx_dist_cos(uu_file);
