8c1f0db298d7c71f14339f2eec6a1f27133af311
yuichiok <yuichi.okugawa@cern.ch>
2023-05-31 22:33:51 +0900

    deleted histograms unnecessary for ccbar analysis

--- NtupleProcessor/include/HistManager.hh
+++ NtupleProcessor/include/HistManager.hh
@@ -24,120 +24,6 @@ class HistManager
 
   // Declear histograms
   // h1 hist
-    enum h1_index {
-      gen_q_cos,
-      gen_q_qcos,
-
-      gen_K_cos,
-      gen_K_qcos,
-
-      cheat_K_cos,
-      cheat_K_qcos,
-
-      reco_K_cos,
-      reco_K_qcos,
-
-      reco_K_scos,
-
-      gen_reco_K_sep_cos,
-
-      reco_sum_jetE,
-      reco_jet_sep,
-
-      lpfo_gen_K_mom,
-      lpfo_reco_K_mom,
-
-      gen_N_K_cos,
-      reco_N_K_cos,
-      N_K_corr_cos,
-
-      gen_N_K_cos2,
-      reco_N_K_cos2,
-      N_K_corr_cos2,
-
-      dummy_h1,
-      Last_h1 = dummy_h1
-    };
-    TH1F * h1[Last_h1];
-
-    enum h1_pq_index {
-      acc_KK,
-      rej_KK,
-
-      dummy_pq,
-      Last_h1_pq = dummy_pq
-    };
-    TH1F * h1_pq[Last_h1_pq];
-
-    enum h1_particle_ratio_index {
-      K_rate_gen,
-      pi_rate_gen,
-      p_rate_gen,
-
-      K_rate_reco,
-      pi_rate_reco,
-      p_rate_reco,
-
-      dummy_particle_ratio,
-      Last_h1_dummy_particle_ratio = dummy_particle_ratio
-    };
-    TH1F * h1_particle_ratio[Last_h1_dummy_particle_ratio];
-
-  // h2 hist
-    enum h2_index {
-
-      gen_K_p_cos,
-      reco_K_p_cos,
-
-      nK_gen_reco,
-      npi_gen_reco,
-      np_gen_reco,
-
-      stable_cos,
-      purity_cos,
-
-      dummy_h2,
-      Last_h2 = dummy_h2
-    };
-    TH2F * h2[Last_h2];
-
-    enum h2_particle_ratio_cos_index {
-      K_rate_cos_gen,
-      pi_rate_cos_gen,
-      p_rate_cos_gen,
-
-      K_rate_cos_reco,
-      pi_rate_cos_reco,
-      p_rate_cos_reco,
-
-      dummy_particle_ratio_cos,
-      Last_h2_dummy_particle_ratio_cos = dummy_particle_ratio_cos
-    };
-    TH2F * h2_particle_ratio_cos[Last_h2_dummy_particle_ratio_cos];
-
-    enum h2_dEdx_index {
-
-      gen_K_dEdx_p,
-      gen_pi_dEdx_p,
-      gen_p_dEdx_p,
-
-      gen_K_reco_K_dEdx_p,
-      gen_pi_reco_K_dEdx_p,
-      gen_p_reco_K_dEdx_p,
-
-      gen_K_KdEdx_dist_cos,
-      gen_pi_KdEdx_dist_cos,
-      gen_p_KdEdx_dist_cos,
-
-      reco_K_KdEdx_dist_cos,
-      gen_K_reco_K_KdEdx_dist_cos,
-      gen_pi_reco_K_KdEdx_dist_cos,
-      gen_p_reco_K_KdEdx_dist_cos,
-
-      dummy_dEdx,
-      Last_h2_dEdx = dummy_dEdx
-    };
-    TH2F * h2_dEdx[Last_h2_dEdx];
 
     enum h1_K_reco{
       d0_K_reco_primary,
@@ -237,12 +123,6 @@ class HistManager
     TH1F * h_general[Last_h_general];
 
   private:
-    TList* hList1                = new TList();
-    TList* hList1_pq             = new TList();
-    TList* hList1_particle_ratio = new TList();
-    TList* hList2                = new TList();
-    TList* hList2_dEdx           = new TList();
-
     TList* hList_K_reco          = new TList();
     TList* hList_general         = new TList();
     TList* hList_tagging         = new TList();
--- NtupleProcessor/src/HistManager.cc
+++ NtupleProcessor/src/HistManager.cc
@@ -13,108 +13,6 @@ HistManager::HistManager() {}
 
 void HistManager::InitializeHists()
 {
-  Int_t   cos_bin = 100;
-  Float_t bins_cos_fine[] = {-1.0,-0.98,-0.96,-0.94,-0.92,-0.90,-0.88,-0.86,-0.84,-0.82,-0.80,-0.75,-0.70,-0.60,-0.50,-0.40,-0.30,-0.20,-0.10,
-                            0.0,0.10,0.20,0.30,0.40,0.50,0.60,0.70,0.75,0.80,0.82,0.84,0.86,0.88,0.90,0.92,0.94,0.96,0.98,1.0};
-  Int_t   nbins_cos = sizeof(bins_cos_fine) / sizeof(Float_t) - 1;
-
-  Float_t bins_p[]={0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1,1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8,1.9,2,2.5,3,4,5,6,7,8,9,10,12,14,16,18,20,24,28,32,36,40,44,48,52,56,60,64,68,72,80,90,100};
-  Int_t   nbins_p=sizeof(bins_p)/sizeof(Float_t) - 1;
-
-  Float_t bins_dEdx[200];
-  bins_dEdx[0]=0.1;
-  for(int i=1;i<200;i++) bins_dEdx[i]=bins_dEdx[i-1]+0.1/100.;
-  Int_t nbins_dEdx=199;
-
-  //////////////////
-  //     TH1F     //
-  //////////////////
-    h1[gen_q_cos]       = new TH1F("h_gen_q_cos","; Generated q#bar{q} cos#theta; Entries",cos_bin,-1,1);
-    h1[gen_q_qcos]      = new TH1F("h_gen_q_qcos","; Generated q#bar{q} qcos#theta; Entries",cos_bin,-1,1);
-
-    h1[gen_K_cos]       = new TH1F("h_gen_K_cos","; Generated Kaon cos#theta; Entries",cos_bin,-1,1);
-    h1[gen_K_qcos]      = new TH1F("h_gen_K_qcos","; Generated Kaon qcos#theta; Entries",cos_bin,-1,1);
-    
-    h1[cheat_K_cos]     = new TH1F("h_cheat_K_cos",";LPFO Kaon cos#theta; Entries",cos_bin,-1,1);
-    h1[cheat_K_qcos]    = new TH1F("h_cheat_K_qcos",";LPFO Kaon cos#theta; Entries",cos_bin,-1,1);
-
-    h1[reco_K_cos]      = new TH1F("h_reco_K_cos",";LPFO Kaon cos#theta; Entries",cos_bin,-1,1);
-    h1[reco_K_qcos]     = new TH1F("h_reco_K_qcos",";LPFO Kaon cos#theta; Entries",cos_bin,-1,1);
-    
-    h1[reco_K_scos]     = new TH1F("h_reco_K_scos",";LPFO Kaon cos#theta; Entries",cos_bin,-1,1);
-
-    h1[gen_reco_K_sep_cos] = new TH1F("h_gen_reco_K_sep_cos",";Kaon cos#theta_{gen-reco}; Entries",cos_bin,-1,1);
-
-  // ISR parameters
-    h1[reco_sum_jetE]   = new TH1F("h_reco_sum_jetE", ";Visible Energy (GeV);", 100, 0, 300);
-    h1[reco_jet_sep]    = new TH1F("h_reco_jet_sep", ";Jet sep |cos#theta|;", 100, 0, 1);
-
-    h1[lpfo_gen_K_mom]  = new TH1F("h_lpfo_gen_K_mom","; Leading Gen Kaon momentum (GeV); Entries",100,0,100);
-    h1[lpfo_reco_K_mom] = new TH1F("h_lpfo_reco_K_mom","; Leading Reco Kaon momentum (GeV); Entries",100,0,100);
-
-  // Number of Gen Reco Kaons
-    h1[gen_N_K_cos]     = new TH1F("h_gen_N_K_cos",";cos#theta;Generated N Kaons", cos_bin,-1,1);
-    h1[reco_N_K_cos]    = new TH1F("h_reco_N_K_cos",";cos#theta;Reconstructed N Kaons", cos_bin,-1,1);
-    h1[N_K_corr_cos]    = new TH1F("h_N_K_corr_cos",";cos#theta;Correctly Reconstructed N Kaons", cos_bin,-1,1);
-
-    h1[gen_N_K_cos2]    = new TH1F("h_gen_N_K_cos2",";cos#theta;Generated N Kaons", 100,-1,1);
-    h1[reco_N_K_cos2]   = new TH1F("h_reco_N_K_cos2",";cos#theta;Reconstructed N Kaons", 100,-1,1);
-    h1[N_K_corr_cos2]   = new TH1F("h_N_K_corr_cos2",";cos#theta;Correctly Reconstructed N Kaons", 100,-1,1);
-
-  // pq method
-    h1_pq[acc_KK]      = new TH1F("h_acc_KK_cos",";Accepted K^{+}K^{-} cos#theta;N_{acc}",cos_bin,-1,1);
-    h1_pq[rej_KK]      = new TH1F("h_rej_KK_cos",";Rejected K^{+}K^{-} cos#theta;N_{rej}",cos_bin,-1,1);
-
-  // particle ratio
-    h1_particle_ratio[K_rate_gen]  = new TH1F("h_K_rate_gen",";Ratio of Kaons / Event (gen);Entries",11,0,1.1);
-    h1_particle_ratio[pi_rate_gen] = new TH1F("h_pi_rate_gen",";Ratio of Pions / Event (gen);Entries",11,0,1.1);
-    h1_particle_ratio[p_rate_gen]  = new TH1F("h_p_rate_gen",";Ratio of Protons / Event (gen);Entries",11,0,1.1);
-
-    h1_particle_ratio[K_rate_reco]  = new TH1F("h_K_rate_reco",";Ratio of Kaons / Event (reco);Entries",11,0,1.1);
-    h1_particle_ratio[pi_rate_reco] = new TH1F("h_pi_rate_reco",";Ratio of Pions / Event (reco);Entries",11,0,1.1);
-    h1_particle_ratio[p_rate_reco]  = new TH1F("h_p_rate_reco",";Ratio of Protons / Event (reco);Entries",11,0,1.1);
-
-
-  //////////////////
-  //     TH2F     //
-  //////////////////
-    h2[gen_K_p_cos]  = new TH2F("h2_gen_K_p_cos" ,";cos#theta;p (GeV)",cos_bin,-1,1,100,0,60);
-    h2[reco_K_p_cos] = new TH2F("h2_reco_K_p_cos",";cos#theta;p (GeV)",cos_bin,-1,1,100,0,60);
-
-    h2[nK_gen_reco]  = new TH2F("h2_nK_gen_reco" ,";N Kaons (Reco);N Kaons (Gen)",   50,0,50,50,0,50);
-    h2[npi_gen_reco] = new TH2F("h2_npi_gen_reco",";N Pions (Reco);N Pions (Gen)",   50,0,50,50,0,50);
-    h2[np_gen_reco]  = new TH2F("h2_np_gen_reco",";N Protons (Reco);N Protons (Gen)",50,0,50,50,0,50);
-    
-    h2[stable_cos]   = new TH2F("h2_stable_cos",";cos#theta;Stability",cos_bin,-1,1,50,0,1);
-    h2[purity_cos]   = new TH2F("h2_purity_cos",";cos#theta;Purity",   cos_bin,-1,1,50,0,1);
-
-  // particle ratio
-    h2_particle_ratio_cos[K_rate_cos_gen]   = new TH2F("h2_K_rate_cos_gen",";qcos#theta;Ratio of Kaons / Event (gen)",40,-1,1,11,0,1.1);
-    h2_particle_ratio_cos[pi_rate_cos_gen]  = new TH2F("h2_pi_rate_cos_gen",";qcos#theta;Ratio of Pions / Event (gen)",40,-1,1,11,0,1.1);
-    h2_particle_ratio_cos[p_rate_cos_gen]   = new TH2F("h2_p_rate_cos_gen",";qcos#theta;Ratio of Protons / Event (gen)",40,-1,1,11,0,1.1);
-
-    h2_particle_ratio_cos[K_rate_cos_reco]  = new TH2F("h2_K_rate_cos_reco",";qcos#theta;Ratio of Kaons / Event (reco)",40,-1,1,11,0,1.1);
-    h2_particle_ratio_cos[pi_rate_cos_reco] = new TH2F("h2_pi_rate_cos_reco",";qcos#theta;Ratio of Pions / Event (reco)",40,-1,1,11,0,1.1);
-    h2_particle_ratio_cos[p_rate_cos_reco]  = new TH2F("h2_p_rate_cos_reco",";qcos#theta;Ratio of Protons / Event (reco)",40,-1,1,11,0,1.1);
-
-  // dEdx
-    h2_dEdx[gen_K_dEdx_p]  = new TH2F("h2_gen_K_dEdx_p"  ,";p (GeV);#frac{dE}{dx}",nbins_p,bins_p,nbins_dEdx,bins_dEdx);
-    h2_dEdx[gen_pi_dEdx_p] = new TH2F("h2_gen_pi_dEdx_p" ,";p (GeV);#frac{dE}{dx}",nbins_p,bins_p,nbins_dEdx,bins_dEdx);
-    h2_dEdx[gen_p_dEdx_p]  = new TH2F("h2_gen_p_dEdx_p"  ,";p (GeV);#frac{dE}{dx}",nbins_p,bins_p,nbins_dEdx,bins_dEdx);
-
-    h2_dEdx[gen_K_reco_K_dEdx_p]  = new TH2F("h2_gen_K_reco_K_dEdx_p"  ,";p (GeV);#frac{dE}{dx}",nbins_p,bins_p,nbins_dEdx,bins_dEdx);
-    h2_dEdx[gen_pi_reco_K_dEdx_p] = new TH2F("h2_gen_pi_reco_K_dEdx_p" ,";p (GeV);#frac{dE}{dx}",nbins_p,bins_p,nbins_dEdx,bins_dEdx);
-    h2_dEdx[gen_p_reco_K_dEdx_p]  = new TH2F("h2_gen_p_reco_K_dEdx_p"  ,";p (GeV);#frac{dE}{dx}",nbins_p,bins_p,nbins_dEdx,bins_dEdx);
-
-
-    h2_dEdx[gen_K_KdEdx_dist_cos]  = new TH2F("h2_gen_K_KdEdx_dist_cos"  ,";cos#theta;K #frac{dE}{dx} dist", 100, -1, 1, 100, -20, 20);
-    h2_dEdx[gen_pi_KdEdx_dist_cos] = new TH2F("h2_gen_pi_KdEdx_dist_cos" ,";cos#theta;K #frac{dE}{dx} dist", 100, -1, 1, 100, -20, 20);
-    h2_dEdx[gen_p_KdEdx_dist_cos]  = new TH2F("h2_gen_p_KdEdx_dist_cos"  ,";cos#theta;K #frac{dE}{dx} dist", 100, -1, 1, 100, -20, 20);
-
-    h2_dEdx[reco_K_KdEdx_dist_cos]        = new TH2F("h2_reco_K_KdEdx_dist_cos" ,";cos#theta;K #frac{dE}{dx} dist", 100, -1, 1, 100, -20, 20);
-    h2_dEdx[gen_K_reco_K_KdEdx_dist_cos]  = new TH2F("h2_gen_K_reco_K_KdEdx_dist_cos"  ,";cos#theta;K #frac{dE}{dx} dist", 100, -1, 1, 100, -20, 20);
-    h2_dEdx[gen_pi_reco_K_KdEdx_dist_cos] = new TH2F("h2_gen_pi_reco_K_KdEdx_dist_cos" ,";cos#theta;K #frac{dE}{dx} dist", 100, -1, 1, 100, -20, 20);
-    h2_dEdx[gen_p_reco_K_KdEdx_dist_cos]  = new TH2F("h2_gen_p_reco_K_KdEdx_dist_cos"  ,";cos#theta;K #frac{dE}{dx} dist", 100, -1, 1, 100, -20, 20);
 
   // Own
 
@@ -211,29 +109,6 @@ void HistManager::InitializeHists()
 
 void HistManager::Hist2List()
 {
-  for (auto ih : h1) {
-    hList1->Add(ih);
-  }
-
-  for (auto ih : h1_pq) {
-    hList1_pq->Add(ih);
-  }
-
-  for (auto ih : h1_particle_ratio) {
-    hList1_particle_ratio->Add(ih);
-  }
-
-  for (auto ih : h2) {
-    hList2->Add(ih);
-  }
-  
-  for (auto ih : h2_particle_ratio_cos) {
-    hList1_particle_ratio->Add(ih);
-  }
-
-  for (auto ih : h2_dEdx) {
-    hList2_dEdx->Add(ih);
-  }
 
   for (auto ih : h1_K_reco) {
     hList_K_reco->Add(ih);
@@ -257,40 +132,17 @@ void HistManager::WriteLists( TFile * output)
 {
   output->cd(); 
   //Own histos
-  TDirectory * d_own = output->mkdir("Own");
-    d_own->cd();
-    hList_general->Write();
+  hList_general->Write();
   
-  TDirectory * d_reco = d_own->mkdir("Reconstructed");
+  TDirectory * d_reco = output->mkdir("Reconstructed");
     d_reco->cd();
     hList_K_reco->Write();
-  TDirectory * d_PS = d_own->mkdir("PS");
+  TDirectory * d_PS = output->mkdir("PS");
     d_PS->cd();
     hList_PS->Write();
 
-  TDirectory * d_tagging = d_own->mkdir("tagging");
+  TDirectory * d_tagging = output->mkdir("tagging");
     d_tagging->cd();
     hList_tagging->Write();
   
-
-  // Focus to this file
-  output->cd();  
-  // Write histogram lists
-  hList1->Write();
-  hList2->Write();
-
-  TDirectory * d_pq = output->mkdir("pq");
-    d_pq->cd();
-    hList1_pq->Write();
-    output->cd();
-
-  TDirectory * d_particle_ratio = output->mkdir("particle_ratio");
-    d_particle_ratio->cd();
-    hList1_particle_ratio->Write();
-    output->cd();
-
-  TDirectory * d_dEdx = output->mkdir("dEdx");
-    d_dEdx->cd();
-    hList2_dEdx->Write();
-    output->cd();
 }
