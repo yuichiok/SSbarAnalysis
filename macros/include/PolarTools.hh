7863b17366c4fc1d46d9b21839ea31787509bbcd
yuichiok <okugawa@epx.phys.tohoku.ac.jp>
2023-08-31 17:08:26 +0200

    efficiency correction now done using all modes (e, mu, K, Pi, p)

--- macros/include/PolarTools.hh
+++ macros/include/PolarTools.hh
@@ -170,12 +170,16 @@ TH1F * CorrectHist( TString prodMode, TH1F * h_reco, vector<Float_t> p_vec)
 
 TH1F * efficiencyCorrection( TH1F * h, TString LPFO_mode, TFile * file )
 {
-  TString dirName = "efficiency/dEdx/";
-  TH2F *h2_reco_offset = (TH2F*) file->Get(dirName + "h2_reco_" + LPFO_mode + "_" + LPFO_mode + "_momentum_dEdx_dist_cos");
-  TH2F *h2_reco_PID    = (TH2F*) file->Get(dirName + "h2_reco_" + LPFO_mode + "_" + LPFO_mode + "_SPFO_dEdx_dist_cos");
+  TString dirName = "efficiency/";
+  TH1F *h_reco_offset = (TH1F*) file->Get(dirName + "h_reco_" + LPFO_mode + "_momentum");
+  TH1F *h_reco_PID    = (TH1F*) file->Get(dirName + "h_reco_" + LPFO_mode + "_charge");
 
-  TH1F *h_reco_offset = (TH1F*) h2_reco_offset->ProjectionX();
-  TH1F *h_reco_PID    = (TH1F*) h2_reco_PID->ProjectionX();
+  // TString dirName = "efficiency/dEdx/";
+  // TH2F *h2_reco_offset = (TH2F*) file->Get(dirName + "h2_reco_" + LPFO_mode + "_" + LPFO_mode + "_momentum_dEdx_dist_cos");
+  // TH2F *h2_reco_PID    = (TH2F*) file->Get(dirName + "h2_reco_" + LPFO_mode + "_" + LPFO_mode + "_SPFO_dEdx_dist_cos");
+
+  // TH1F *h_reco_offset = (TH1F*) h2_reco_offset->ProjectionX();
+  // TH1F *h_reco_PID    = (TH1F*) h2_reco_PID->ProjectionX();
 
   TH1F *h_eff = (TH1F*) h_reco_PID->Clone();
   h_eff->Sumw2();
