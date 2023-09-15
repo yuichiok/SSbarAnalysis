711189b3155acaba500e734e5718796acb263c54
yuichiok <yuichi.okugawa@cern.ch>
2023-09-15 21:37:25 +0900

    use dictionary to select processing mode

--- batch_jobs/runDir.py
+++ batch_jobs/runDir.py
@@ -1,17 +1,12 @@
 import os
+import sys
 import glob
 import subprocess
+import argparse
+from processDict import production
+from pathlib import Path
 
-# chiral
-# chiral = "eLpR"
-chiral = "eRpL"
-
-# processID
-processIDs = []
-if chiral == "eLpR":
-  processIDs = [15162,15271,15273,15315,15319,15351,15353,15355,15357,15463,15465,15467,15469,15471,15473,15475,15477,15479,15499,15501,15503,15505,15507,15509,15511,15513,15515,15517,15539,15541,15549,15551,15553,15555,15557,15559,15561,15563,15585,15587,15589,15591,15593,15595,15597,15599,15601,15603,15629,15633]
-elif chiral == "eRpL":
-  processIDs = [15165,15275,15277,15279,15359,15361,15363,15365,15367,15369,15481,15483,15485,15487,15489,15491,15493,15495,15497,15519,15521,15523,15525,15527,15529,15531,15533,15535,15537,15565,15567,15569,15571,15573,15575,15577,15579,15581,15583,15605,15607,15609,15611,15613,15616,15618,15620,15625,15627,15631]
+projectDir = Path(__file__).parent.absolute()
 
 subprocess.run('cp ../main.exe ./',shell=True)
 subprocess.run('cp ../etc/* ./etc',shell=True)
@@ -24,12 +19,29 @@ isAll = False
 if nlast_set <= 0:
   isAll = True
 
-for processID in processIDs:
-  data_input = f"/group/ilc/users/yokugawa/QQbar250/l5/{chiral}/{processID}/dEdx_corr/QQbarProcessor_out/"
+parser = argparse.ArgumentParser(description='Run QQbarAnalysis')
+parser.add_argument('--process', type=str, required=True,
+                    help='Production process (P2f_z_h, P4f_ww_h, P4_zz_h, Pe1e1h)')
+parser.add_argument('--chiral',  type=str, required=True,
+                    help='Polarization of beam (eLpR or eRpL)')
 
-  file_list = []
+args  = parser.parse_args()
+
+model = "l5"
+processID  = production[args.process, args.chiral][0]
+prodIDList = production[args.process, args.chiral][1]
+
+print(args.process, args.chiral, processID)
+print(prodIDList)
+
+# for processID in processIDs:
+for prodID in prodIDList:
+  data_input = f"/group/ilc/users/yokugawa/QQbar250/{model}/{args.process}/{args.chiral}/{processID}/{prodID}/dEdx_corr/QQbarProcessor_out/"
+  if not os.path.isdir(data_input):
+    sys.exit(f'Directory {data_input} does not exist.')
+
   try:
-    pattern = os.path.join(data_input, f'*{processID}*.root')
+    pattern = os.path.join(data_input, f'*I{processID}*{prodID}*.root')
     file_list = sorted(glob.glob(pattern))
   except subprocess.CalledProcessError as e:
     print(f"Error: {e}")
@@ -43,7 +55,7 @@ for processID in processIDs:
     nlast = nf
     
   njobs = (nlast - nfirst) // nrun + 1
-  print("processID {}: nlast {}, nfirst {}, nrun {}, njobs {}".format(processID, nlast, nfirst, nrun, njobs))
+  print("processID {}, prodID {}: nlast {}, nfirst {}, nrun {}, njobs {}".format(processID,prodID, nlast, nfirst, nrun, njobs))
   seqlist = range(1, njobs + 1)
   for seq in seqlist:
     nrun0 = nfirst + nrun * (seq - 1)
@@ -54,13 +66,13 @@ for processID in processIDs:
     else:
       nrun1 = nlast
 
-    print(processID, nrun0, nrun1, nrun, njobs)
+    print(prodID, nrun0, nrun1, nrun, njobs)
 
     sub_flist = file_list[nrun0 - 1:nrun1]
     arg_flist = ",".join(sub_flist)
 
-    if processID == 15271 or processID == 15275:
-      log = f"./sublog/{processID}_{seq:03d}.log"
-      subprocess.run(['bsub', '-q', 's', '-J', f'ana_{processID}_{seq:03d}', '-o', log, f'python3', 'runROOT.py', '--flist', arg_flist])
-    else:
-      subprocess.run(['bsub', '-q', 's', '-J', f'ana_{processID}_{seq:03d}', f'python3', 'runROOT.py', '--flist', arg_flist])
+    # if prodID == 15271 or prodID == 15275:
+    #   log = f"./sublog/{prodID}_{seq:03d}.log"
+    #   subprocess.run(['bsub', '-q', 's', '-J', f'ana_{prodID}_{seq:03d}', '-o', log, f'python3', 'runROOT.py', '--flist', arg_flist])
+    # else:
+    #   subprocess.run(['bsub', '-q', 's', '-J', f'ana_{prodID}_{seq:03d}', f'python3', 'runROOT.py', '--flist', arg_flist])
