d240d0fdcc92cfd6a43309ec5d558be4879ca57e
yuichiok <okugawa@epx.phys.tohoku.ac.jp>
2023-10-09 19:32:48 +0200

    plot cummulative dedx

--- macros/dedx/dedx.cc
+++ macros/dedx/dedx.cc
@@ -15,6 +15,8 @@ TString prod_mode = "ss";
 TString chiral    = "eL.pR";
 TString LPFO_mode = "Pi";
 
+TString prod_modes[5] = {"dd","uu","ss","cc","bb"};
+
 // TFile *file = new TFile("../../rootfiles/merged/rv02-02.sv02-02.mILD_l5_o1_v02.E250-SetA.I500010.P2f_z_h." + chiral + "." + prod_mode + ".KPiLPFO.dedxPi.PFOp15.LPFOp15_pNaN.tpc0.mix_uds.correctDist.all.root","READ");
 TFile *file = new TFile("../../rootfiles/merged/rv02-02.sv02-02.mILD_l5_o1_v02.E250-SetA.I500010.P2f_z_h.eL.pR.KPiLPFO.dedxPi.PFOp15.LPFOp15_pNaN.all.root","READ");
 
@@ -34,40 +36,44 @@ TH1F* plotEfficiency(TH1F *h_num, TH1F *h_denom)
   return h_eff;
 }
 
-unordered_map< TString, unordered_map< TString, unordered_map< TString, unordered_map< TString, unordered_map< TString, TH2F* > > > > > h2_dEdx_eff;  // [GenReco][LPFO][TruthID][cut][hist]
+unordered_map< TString, unordered_map< TString, unordered_map< TString, unordered_map< TString, unordered_map< TString, unordered_map< TString, TH2F* > > > > > > h2_dEdx_eff;  // [prod_mode][GenReco][LPFO][TruthID][cut][hist]
 vector<TString> gen_reco  = {"gen","reco"};
 vector<TString> cut_name = {"nocut","momentum", "tpc_hits", "offset", "PID", "SPFO", "charge"};
 vector<TString> heff_dedx_name = {"dEdx_p","dEdx_cos","dEdx_error_cos","dEdx_dist_cos"};
 
-unordered_map< TString, unordered_map< TString, unordered_map< TString, TH2F* > > > h2_dEdx; // [LPFO][TruthID][hist]
+unordered_map< TString, unordered_map< TString, unordered_map< TString, unordered_map< TString, TH2F* > > > > h2_dEdx; // [prod_mode][LPFO][TruthID][hist]
 vector<TString> hdEdx_name = {"dEdx_p","dEdx_cos","dEdx_dist_cos"};
 
 void getHistograms()
 {
-  TString dir_eff_name = prod_mode + "/efficiency/dEdx/";
 
-  for ( auto igen_reco : gen_reco ){
-    for ( auto imode : PFO_mode ){
-      for ( auto itype : PFO_type ){
-        for ( auto icut : cut_name ){
-          for ( auto ihist : heff_dedx_name ){
-            TString hname = "h2_" + prod_mode + "_" + igen_reco + "_" + imode + "_" + itype + "_" + icut + "_" + ihist;
-            TH2F *h = (TH2F*) file->Get(dir_eff_name + hname);
-            h2_dEdx_eff[igen_reco][imode][itype][icut][ihist] = h;
+  for (auto iprod_mode : prod_modes ){
+    TString dir_eff_name = iprod_mode + "/efficiency/dEdx/";
+    for ( auto igen_reco : gen_reco ){
+      for ( auto imode : PFO_mode ){
+        for ( auto itype : PFO_type ){
+          for ( auto icut : cut_name ){
+            for ( auto ihist : heff_dedx_name ){
+              TString hname = "h2_" + iprod_mode + "_" + igen_reco + "_" + imode + "_" + itype + "_" + icut + "_" + ihist;
+              TH2F *h = (TH2F*) file->Get(dir_eff_name + hname);
+              h2_dEdx_eff[iprod_mode][igen_reco][imode][itype][icut][ihist] = h;
+            }
           }
         }
       }
     }
   }
 
-  TString dir_dedx_name = "dEdx/";
 
-  for ( auto imode : PFO_mode ){
-    for ( auto itype : PFO_type ){
-      for ( auto icut : hdEdx_name ){
-        TString hname = "h2_" + imode + "_" + itype + "_" + icut;
-        TH2F *h = (TH2F*) file->Get(dir_dedx_name + hname);
-        h2_dEdx[imode][itype][icut] = h;
+  for (auto iprod_mode : prod_modes ){
+    TString dir_dedx_name = iprod_mode + "/dEdx/";
+    for ( auto imode : PFO_mode ){
+      for ( auto itype : PFO_type ){
+        for ( auto icut : hdEdx_name ){
+          TString hname = "h2_" + iprod_mode + "_" + imode + "_" + itype + "_" + icut;
+          TH2F *h = (TH2F*) file->Get(dir_dedx_name + hname);
+          h2_dEdx[iprod_mode][imode][itype][icut] = h;
+        }
       }
     }
   }
@@ -78,25 +84,42 @@ void dedxP()
   TCanvas *c_dedx_p_type = new TCanvas("c_dedx_p_type", "c_dedx_p_type", 800,800);
   TPad *pad_dedx_p_type  = new TPad("pad_dedx_p_type", "pad_dedx_p_type",0,0,1,1);
   StylePad(pad_dedx_p_type,0,0.15,0,0.17);
+  TLegend *leg = new TLegend(0.5,0.76,0.75,0.85);
+  leg->SetLineColor(0);
 
-  int count = 0;
+  int count_draw = 0;
   for ( auto itype : PFO_type ){
 
-    TH2F *h_dedx_p = h2_dEdx_eff.at("reco").at(LPFO_mode).at(itype).at("nocut").at("dEdx_p");
-    h_dedx_p->SetFillColor(type_color_map[itype]);
-    h_dedx_p->SetLineColor(type_color_map[itype]);
-    h_dedx_p->GetXaxis()->SetRangeUser(15,100);
+    TH2F *h_dedx_p;
+    Int_t countAdd = 0;
 
-    cout << itype << " " << h_dedx_p->GetEntries() << endl;
+    for ( auto iprod_mode : prod_modes ){
 
-    if(count) h_dedx_p->Draw("box same");
+      TH2F *h_dedx_p_tmp = h2_dEdx_eff.at(prod_mode).at("reco").at(LPFO_mode).at(itype).at("nocut").at("dEdx_p");
+      h_dedx_p_tmp->SetFillColor(type_color_map[itype]);
+      h_dedx_p_tmp->SetLineColor(type_color_map[itype]);
+      h_dedx_p_tmp->GetXaxis()->SetRangeUser(15,100);
+
+      if(countAdd){
+        h_dedx_p->Add(h_dedx_p_tmp);
+      }else{
+        h_dedx_p = (TH2F*) h_dedx_p_tmp->Clone();
+      }
+
+      countAdd++;
+    }
+
+    if(count_draw) h_dedx_p->Draw("box same");
     else{
       h_dedx_p->SetTitle(";p [GeV];dE/dx #times 10^{-6} GeV/mm");
       h_dedx_p->Draw("box");
     }
+    leg->AddEntry(h_dedx_p,itype,"f");
+    count_draw++;
 
-    count++;
   }
+
+  leg->Draw();
 
 
 }
@@ -130,9 +153,9 @@ void dedxDistCosProjType()
       count++;
 
       TString hTitle = itype + " | " + iname;
-      TH2F *h_dedx_cos           = h2_dEdx_eff.at("reco").at(LPFO_mode).at(itype).at(iname).at("dEdx_cos");
-      TH2F *h_dedx_p             = h2_dEdx_eff.at("reco").at(LPFO_mode).at(itype).at(iname).at("dEdx_p");
-      TH2F *h_dedx_dist_cos      = h2_dEdx_eff.at("reco").at(LPFO_mode).at(itype).at(iname).at("dEdx_dist_cos");
+      TH2F *h_dedx_cos           = h2_dEdx_eff.at(prod_mode).at("reco").at(LPFO_mode).at(itype).at(iname).at("dEdx_cos");
+      TH2F *h_dedx_p             = h2_dEdx_eff.at(prod_mode).at("reco").at(LPFO_mode).at(itype).at(iname).at("dEdx_p");
+      TH2F *h_dedx_dist_cos      = h2_dEdx_eff.at(prod_mode).at("reco").at(LPFO_mode).at(itype).at(iname).at("dEdx_dist_cos");
       TH1F *h_dedx_dist_cos_proj = (TH1F*) h_dedx_dist_cos->ProjectionX();
 
       h_dedx_dist_cos->SetTitle(hTitle);
@@ -186,7 +209,7 @@ void dedxDistCosProj()
   TH1F * h_dedx_dist_cos_proj_sum = new TH1F("h_dedx_dist_cos_proj_sum","h_dedx_dist_cos_proj_sum",100,-1,1);
   StyleHist(h_dedx_dist_cos_proj_sum,kBlue);
   for ( auto itype : PFO_type ){
-    TH2F *h_dedx_dist_cos = h2_dEdx.at(LPFO_mode).at(itype).at("dEdx_dist_cos");
+    TH2F *h_dedx_dist_cos = h2_dEdx.at(prod_mode).at(LPFO_mode).at(itype).at("dEdx_dist_cos");
     TH1F *h_dedx_dist_cos_proj = (TH1F*) h_dedx_dist_cos->ProjectionX();
     StyleHist(h_dedx_dist_cos_proj,kBlue);
 
@@ -211,7 +234,7 @@ void dedxCos()
 
     count++;
 
-    TH2F *h_dedx_cos = h2_dEdx.at(LPFO_mode).at(itype).at("dEdx_cos");
+    TH2F *h_dedx_cos = h2_dEdx.at(prod_mode).at(LPFO_mode).at(itype).at("dEdx_cos");
 
     c_type_dedx_cos->cd(count);
     h_dedx_cos->Draw("colz");
@@ -238,8 +261,8 @@ void dedxOffsetProjection()
     Color_t icolor = ipair.second;
 
     count++;
-    TH2F *h_dedx_p   = h2_dEdx_eff.at("reco").at(LPFO_mode).at(itype).at("offset").at("dEdx_p");
-    TH2F *h_dedx_cos = h2_dEdx_eff.at("reco").at(LPFO_mode).at(itype).at("offset").at("dEdx_cos");
+    TH2F *h_dedx_p   = h2_dEdx_eff.at(prod_mode).at("reco").at(LPFO_mode).at(itype).at("offset").at("dEdx_p");
+    TH2F *h_dedx_cos = h2_dEdx_eff.at(prod_mode).at("reco").at(LPFO_mode).at(itype).at("offset").at("dEdx_cos");
     TH1F *h_dedx_p_proj   = (TH1F*) h_dedx_p->ProjectionY();
     StyleHist(h_dedx_p_proj,icolor);
 
@@ -268,7 +291,7 @@ void dedxOffsetMeanSigma()
     TString itype  = ipair.first;
     Color_t icolor = ipair.second;
 
-    TH2F *h_dedx_cos = h2_dEdx_eff.at("reco").at(LPFO_mode).at(itype).at("offset").at("dEdx_cos");
+    TH2F *h_dedx_cos = h2_dEdx_eff.at(prod_mode).at("reco").at(LPFO_mode).at(itype).at("offset").at("dEdx_cos");
 
     Int_t nbins = h_dedx_cos->GetNbinsX();
     Float_t tge_cos[nbins], tge_err_cos[nbins];
@@ -350,12 +373,12 @@ void dedx()
 
   getHistograms();
 
-  // dedxP();
+  dedxP();
   // dedxDistCosProj();
   // dedxDistCosProjType();
   // dedxCos();
   // dedxOffsetProjection();
-  dedxOffsetMeanSigma();
+  // dedxOffsetMeanSigma();
 
 
 }
