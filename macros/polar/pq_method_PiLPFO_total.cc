e39c3d54757ab89aea9a7e11f9ece9d3da3823f5
yuichiok <okugawa@epx.phys.tohoku.ac.jp>
2024-02-08 12:00:07 +0100

    change range

--- macros/polar/pq_method_PiLPFO_total.cc
+++ macros/polar/pq_method_PiLPFO_total.cc
@@ -12,7 +12,7 @@ using std::vector; using std::array; using std::unordered_map;
 // TString chiral    = "eR.pL";
 TString LPFO_mode = "Pi";
 // Float_t TopRange = 700E3;
-Float_t TopRange = 0.7;
+Float_t TopRange = 10;
 
 TString inputDir = "../../rootfiles/merged/";
 array<TString,2> chirals   = {"eL.pR", "eR.pL"};
